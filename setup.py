fb5919efae44442975cad05273a268cfcf6d14b2
Frank Dresel <frankdressel@users.noreply.github.com>
2018-05-21 21:50:21 +0000

    Fixed repo path.

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ setup(
     #
     # This field corresponds to the "Home-Page" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#home-page-optional
-    url='https://github.com/frankdressel/jupyter-dygraph',  # Optional
+    url='https://github.com/frankdressel/jupyter_dygraphs',  # Optional
 
     # This should be your name or the name of the organization which owns the
     # project.
